48655bcbcef77038ba611a2d638c7cd2d8a48498
Amber Williams <awilliams1234@live.com>
2020-02-25 01:22:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,13 @@
 # Your code goes here!
-class Dog 
+class Dog
+  
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
-    def bark 
-  puts "woof!"
-end
-end
-
-def name 
-  @this_dogs_name
-end
-end
-
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
 
 
   
